8fbaa90fc5ab2b2aafb4f775909886c6d87cf959
gmartin <gmartincasals@gmail.com>
2017-04-11 23:02:23 +0200

    albums

---
+++ api-restful-albums/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3700;
+
+mongoose.connect('mongodb://localhost:27017/app_albums', (err, res) => {
+	if(err){
+		throw err;
+	}else{
+		console.log("Base de datos correctamente....");
+
+		app.listen(port, function(){
+			console.log("API RESTful de albums escuchando....")
+
+
+		});
+
+	}
+
+
+});	
